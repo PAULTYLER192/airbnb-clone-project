f5d71018b8ebd67f3cd091a48a3aa64e2da4e7dc
PAULTYLER192 <ochiengpaul193@gmail.com>
2025-07-07 08:55:17 +0300

    Initial setup: Initialized Django project, installed dependencies, set up PostgreSQL with airbnb_user on port 5432, and created listings app

--- airbnb/airbnb/settings.py
+++ airbnb/airbnb/settings.py
@@ -72,11 +72,15 @@ WSGI_APPLICATION = 'airbnb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+from decouple import config
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': config('DATABASE_PORT', default='5432'),
     }
 }
 
